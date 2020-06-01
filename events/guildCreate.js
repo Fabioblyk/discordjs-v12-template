a0ce721942ff04418829d49b09f327c07ce616ac
Barış Demirci <demirci.baris38@gmail.com>
2020-06-01 09:58:35 +0300

    👾 Oops we made a mistake

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 // triggered when bot joins a guild
 module.exports = async guild => {
     await db.set(`${guild.id}.language`, "en"); // set default language to english
-    guild.language = "en"; // set default language to english
+    guild.language = require("../locales/en.json"); // set default language to english
     console.log(`[JOINED GUILD]: ${guild.name} | ${guild.id}`); // Show an informative on console
 }
