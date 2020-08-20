ccca23f79b142f2fda8595b9d6434d169595a3b5
Barış Demirci <barbarbar338@picass.co>
2020-08-20 10:53:09 +0300

    👀 Fixed language save problem

--- commands/moderator/language.js
+++ commands/moderator/language.js
@@ -6,7 +6,7 @@ module.exports = {
     usage: "language <code>",
     description: "language_command",
     aliases: [ "lang" ],
-    cooldown: 15,
+    cooldown: 2,
     enabled: true,
     permissions: [ "BAN_MEMBERS" ],
     exec: async (client, message, args) => {
@@ -14,11 +14,14 @@ module.exports = {
         if (!lang) return await message.channel.send(message.guild.language.specify_language);
         let languages = fs.readdirSync("./locales/").filter(file => file.endsWith(".json")).map(file => file.replace(".json", ""));
         if (!languages.includes(lang)) return await message.channel.send(message.guild.language.specify_valid_language.replace(/{languages}/g, languages.join(", ")));
-        await guildModel.updateOne({
+        let guildDocument = await guildModel.findOne({
             guildID: message.guild.id
-        }, {
-            language: lang
         });
+        if (!guildDocument) guildDocument = new guildModel({
+            guildID: message.guild.id
+        });
+        guildDocument.language = lang;
+        await guildDocument.save();
         message.guild.language = require(`../../locales/${lang}.json`);
         await message.channel.send(message.guild.language.language_updated);
     }
